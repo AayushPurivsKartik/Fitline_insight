2ee268cd44fcd3a9a8f565dae92389ae91ebd1b2
AayushPurivsKartik <aayushpuri683@gmail.com>
2024-10-20 13:57:05 +0530

    added new features

--- index.js
+++ index.js
@@ -122,7 +122,9 @@ app.get('/ayurveda',(req,res)=>{
 app.get('/chatbot',(req,res)=>{
     res.render('chatbot');
 });
-
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+});
 // for error handling
 app.all('*',(req,res,next)=>{
     next(new ExpressError(404,'Page not found'));
